aedec4cc7066aba8f40daa4dfe4c35077e101cfe
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2023-11-10 09:43:21 +0530

    Reverse String Program

--- src/testngbasics/TestGit.java
+++ src/testngbasics/TestGit.java
@@ -1,10 +1,16 @@
 package testngbasics;
 
 public class TestGit {
-
-	public static void main(String[] args) {
-		System.out.println("Just to check working git");
-
-	}
+	  public static void main(String[] args) {
+		    String originalStr = "Hello Atul";
+		    String reversedStr = "";
+		    System.out.println("Original string: " + originalStr);
+		        
+		    for (int i = 0; i < originalStr.length(); i++) {
+		      reversedStr = originalStr.charAt(i) + reversedStr;
+		    }
+		    
+		    System.out.println("Reversed string: "+ reversedStr);
+		  }
 
 }
